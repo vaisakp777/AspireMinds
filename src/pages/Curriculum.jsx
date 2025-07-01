ada119fff51f1623323d1cc9cc7c2397d0d9e1dc
vaisakp777 <122760141+vaisakp777@users.noreply.github.com>
2025-07-01 06:13:23 +0530

    Update Curriculum.jsx

--- src/pages/Curriculum.jsx
+++ src/pages/Curriculum.jsx
@@ -22,19 +22,19 @@ const Curriculum = () => {
           
           </div>
           <div className="level-image-placeholder">
-            <img src={imageUrl+"coaching.jpg"} alt="coaching image" className='coachingImg'></img>
+            <img src="../../images/coaching.jpg" alt="coaching image" className='coachingImg'></img>
           </div>
         </div>
         <div className="curriculum-grid">
           <div className="curriculum-card secondary">
-            <div className="card-icon-placeholder"><img src={imageUrl+"peaceful.jpg"} alt="System Access icon placeholder" className='card-icon'></img></div>
+            <div className="card-icon-placeholder"><img  src="../../images/peaceful.jpg"  alt="System Access icon placeholder" className='card-icon'></img></div>
             <h3>
             Helping teens overcome stress, anxiety, and concentration issues while boosting focus, confidence, and motivation in academics.</h3>
            
           </div>
         
           <div className="curriculum-card primary">
-            <div className="card-icon-placeholder"><img src={imageUrl+"positive.jpg"} alt="Hypnosis icon placeholder" className='card-icon'></img></div>
+            <div className="card-icon-placeholder"><img  src="../../images/positive.jpg"  alt="Hypnosis icon placeholder" className='card-icon'></img></div>
             <h3>
             Break unwanted habits like procrastination, overeating, nail-biting, etc., and install empowering new patterns of behavior.
             </h3>
@@ -48,7 +48,7 @@ const Curriculum = () => {
           {/* Psycho-Education Block 3 */}
           <div className="curriculum-card primary">
             <div className="card-icon-placeholder">
-              <img src={imageUrl+"transformation.jpg"} alt="transoformation icon placeholder" className='card-icon'></img>
+              <img  src="../../images/transformation.jpg" alt="transoformation icon placeholder" className='card-icon'></img>
             </div>
             <h3>
             Overcome self-doubt, fear of judgment, and inner criticism. Step into a confident, authentic version of yourself.
@@ -69,14 +69,14 @@ const Curriculum = () => {
         <div className="curriculum-grid">
           {/* Diagnostic Techniques */}
           <div className="curriculum-card secondary">
-            <div className="card-icon-placeholder"><img src={imageUrl+"assess.jpg"} alt="Diagnostic icon placeholder" className='card-icon'></img></div>
+            <div className="card-icon-placeholder"><img  src="../../images/assess.jpg" alt="Diagnostic icon placeholder" className='card-icon'></img></div>
             <h3>Identify and shift limiting thoughts and deep-rooted beliefs that keep you stuck—align your mind with your goals and values.
             </h3>
          
           </div>
                     {/* Advanced Psycho-Education */}
           <div className="curriculum-card primary">
-            <div className="card-icon-placeholder"><img src={imageUrl+"path.jpg"} alt="Meditation icon placeholder" className='card-icon'></img></div>
+            <div className="card-icon-placeholder"><img src="../../images/path.jpg" alt="Meditation icon placeholder" className='card-icon'></img></div>
             <h3>
             Clarify your life and career goals, remove mental blocks, and stay motivated with a clear subconscious alignment.
 
@@ -88,7 +88,7 @@ const Curriculum = () => {
         <div className="curriculum-grid">
                     {/* Change Processes */}
           <div className="curriculum-card primary">
-            <div className="card-icon-placeholder"><img src={imageUrl+"change.jpg"} alt="Change Processes icon placeholder" className='card-icon'></img></div>
+            <div className="card-icon-placeholder"><img  src="../../images/change.jpg" alt="Change Processes icon placeholder" className='card-icon'></img></div>
             <h3>Special support for women dealing with life transitions—career changes, motherhood, empty nest, or midlife re-evaluation—with empathy and empowerment.
           </h3>
           
@@ -98,7 +98,7 @@ const Curriculum = () => {
       
           <div className="curriculum-card secondary">
             <div className="card-icon-placeholder">
-              <img src={imageUrl+"brain.jpg"} alt="Brain icon placeholder" className='card-icon'></img>
+              <img src="../../images/brain.jpg" alt="Brain icon placeholder" className='card-icon'></img>
             </div>
             <h3>
               Release chronic stress, anxiety, and emotional overload using deep relaxation, anchoring, and subconscious reprogramming techniques
